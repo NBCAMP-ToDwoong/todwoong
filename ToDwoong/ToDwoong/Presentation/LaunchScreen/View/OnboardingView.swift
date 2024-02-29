21a12ca82613ddeab4a002f081612eb84d419ed9
codeIsGone <151711359+codeIsGone@users.noreply.github.com>
2024-02-29 15:26:02 +0900

    [Feat] #13 - 온보딩 페이지 권한 요청 기능 구현 (#14)
    * [Feat]#13 - 온보딩 페이지 UI 및 레이아웃 구현
    - View 분리하여 ViewController와 연결
    - View UI 프로퍼티 생성 및 레이아웃 설정
    
    * [Feat]#13 - 온보딩 페이지 권한 요청 기능 구현
    - info에 key 추가
    - 알림 권한 요청 구현
    - 위치 정보 접근 권한 요청 구현
    
    * [Chore]#13 - 접근제어자 추가
    
    * [Feat]#13 - 온보딩 페이지 PR 리뷰 반영
    - 필요없는 주석 삭제
    - 마크 주석 하단 공백 추가
    - import 순서 변경
    
    * [Feat]#13 - 린트 적용
    - MARK 띄어쓰기
    - 카멜케이스 준수

---
+++ ToDwoong/ToDwoong/Presentation/LaunchScreen/View/OnboardingView.swift
@@ -0,0 +1,81 @@
+//
+//  OnboardingView.swift
+//  ToDwoong
+//
+//  Created by 홍희곤 on 2/28/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class OnboardingView: UIView {
+    
+    // MARK: UI Properties
+    
+    private lazy var locationImageView: UIImageView = {
+        let imageView = UIImageView()
+//        imageView.image = UIImage()
+        
+        return imageView
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "위치권한 항상 허용 상태"
+        
+        return label
+    }()
+    
+    lazy var requestButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("시작하기", for: .normal)
+        button.backgroundColor = .green
+        
+        return button
+    }()
+    
+    // MARK: Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+// MARK: Extension
+
+extension OnboardingView {
+    private func setUI() {
+        self.backgroundColor = .white
+        
+        [
+            locationImageView,
+            descriptionLabel,
+            requestButton
+        ].forEach{self.addSubview($0)}
+        
+        locationImageView.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-50)
+            make.height.equalTo(50)
+            make.width.equalTo(30)
+        }
+        
+        descriptionLabel.snp.makeConstraints{ make in
+            make.top.equalTo(locationImageView.snp_bottomMargin).offset(16)
+            make.centerX.equalToSuperview()
+        }
+        
+        requestButton.snp.makeConstraints{ make in
+            make.bottom.equalTo(safeAreaLayoutGuide).offset(-30)
+            make.leading.equalTo(safeAreaLayoutGuide).offset(16)
+            make.trailing.equalTo(safeAreaLayoutGuide).offset(-16)
+        }
+    }
+}
---
+++ ToDwoong/ToDwoong/Presentation/LaunchScreen/ViewController/OnboardingViewController.swift
@@ -0,0 +1,89 @@
+//
+//  OnboardingViewController.swift
+//  ToDwoong
+//
+//  Created by 홍희곤 on 2/28/24.
+//
+
+import CoreLocation
+import UIKit
+import UserNotifications
+
+final class OnboardingViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    private let locationManager = CLLocationManager()
+    private let center = UNUserNotificationCenter.current()
+    
+    // MARK: UI Properties
+    
+    private var onboardingView = OnboardingView()
+    
+    // MARK: Life Cycle
+    
+    override func loadView() {
+        view = onboardingView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setAction()
+        setDelegate()
+    }
+}
+
+// MARK: Action
+
+extension OnboardingViewController {
+    
+    private func setAction() {
+        onboardingView.requestButton.addTarget(self, action: #selector(requestButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func requestButtonTapped() {
+        requestAuthorization()
+    }
+}
+
+// MARK: Requset Method
+
+extension OnboardingViewController {
+    private func requestAuthorization() {
+        requestLocationAuthorization()
+        requestNotificationAuthorization()
+    }
+    
+    private func requestLocationAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    private func requestNotificationAuthorization() {
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if granted {
+
+            } else {
+
+            }
+        }
+    }
+}
+
+// MARK: LocationDelegate
+
+extension OnboardingViewController: CLLocationManagerDelegate {
+    
+    private func setDelegate() {
+        locationManager.delegate = self
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:
+            locationManager.requestAlwaysAuthorization()
+        default:
+            break
+        }
+    }
+}
