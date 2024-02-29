5e423ab8e985538e33a82c07a2f2ada26e0de672
Yenny <107637741+yenny42@users.noreply.github.com>
2024-02-29 11:27:45 +0900

    [Setting] #10 - 프로젝트 설정 변경 (#18)
    * [Setting] #10 - Swift Lint 추가
    
    * [Chore] #10 - .swiftlint.yml 내용 변경

--- ToDwoong/ToDwoong/Global/App/AppDelegate.swift
+++ ToDwoong/ToDwoong/Global/App/AppDelegate.swift
@@ -5,26 +5,31 @@
 //  Created by yen on 2/23/24.
 //
 
-import UIKit
 import CoreData
+import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
-
+    
     // MARK: Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Model")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
@@ -34,9 +39,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-        
+    
     // MARK: Core Data Saving support
-        
+    
     func saveContext() {
         let context = persistentContainer.viewContext
         if context.hasChanges {
