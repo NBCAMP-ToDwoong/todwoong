db9b1d2af497bff3caa0053fb891ca1fc26fd519
yenny <yennydev42@gmail.com>
2024-03-03 21:11:26 +0900

    [Chore] #22 - 메서드 분리

--- ToDwoong/ToDwoong/Presentation/Tabbar/TabbarViewController.swift
+++ ToDwoong/ToDwoong/Presentation/Tabbar/TabbarViewController.swift
@@ -7,17 +7,19 @@
 
 import UIKit
 
+import TodwoongDesign
+
 class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = TDStyle.color.bgBlue
     }
 }
 
 class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
+        view.backgroundColor = TDStyle.color.bgRed
     }
 }
 
@@ -25,11 +27,17 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUI()
         setTabBar()
     }
 }
 
 extension TabBarController {
+    private func setUI() {
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = TDStyle.color.mainDarkTheme
+    }
+    
     private func setTabBar() {
         let toDoListViewController = FirstViewController()
         toDoListViewController.tabBarItem = UITabBarItem(title: "할 일", image: UIImage(systemName: "checklist"), tag: 0)
@@ -37,6 +45,8 @@ extension TabBarController {
         let calendarViewController = SecondViewController()
         calendarViewController.tabBarItem = UITabBarItem(title: "캘린더", image: UIImage(systemName: "calendar"), tag: 1)
 
-        viewControllers = [toDoListViewController, calendarViewController].map { UINavigationController(rootViewController: $0) }
+        viewControllers = [toDoListViewController, calendarViewController].map {
+            UINavigationController(rootViewController: $0)
+        }
     }
 }
