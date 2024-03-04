61e48a32126e51d47aa20a14983e97ce41674aa0
Kdy123 <dudtodtls24@gmail.com>
2024-03-05 02:27:02 +0900

    [Feat]#11- GroupListViewCell의 확장을 파일로 분리
    - extension을 파일을 만들어 작성하였습니다.

--- ToDwoong/ToDwoong/Presentation/GroupList/View/GroupListTableViewCell.swift
+++ ToDwoong/ToDwoong/Presentation/GroupList/View/GroupListTableViewCell.swift
@@ -116,31 +116,3 @@ final class EditGroupListTableViewCell: UITableViewCell {
         fatalError("초기화가 구현되지 않았습니다.")
     }
 }
-
-// MARK: Extension
-
-extension UIImage {
-    static func imageWithColor(_ color: UIColor, size: CGSize) -> UIImage {
-        let rect = CGRect(origin: .zero, size: size)
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
-        color.setFill()
-        UIRectFill(rect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return image
-    }
-}
-
-extension UIImage {
-    static func roundedImage(color: UIColor, size: CGSize) -> UIImage {
-        let renderer = UIGraphicsImageRenderer(size: size)
-        return renderer.image { context in
-            let roundedRect = CGRect(origin: .zero, size: size).insetBy(dx: 4, dy: 4)
-            context.cgContext.setFillColor(color.cgColor)
-            context.cgContext.setStrokeColor(UIColor.clear.cgColor)
-            context.cgContext.setLineWidth(0)
-            context.cgContext.addEllipse(in: roundedRect)
-            context.cgContext.drawPath(using: .fillStroke)
-        }
-    }
-}
