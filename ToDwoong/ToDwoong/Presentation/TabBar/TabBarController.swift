2d6738bcf03af3432b9bda76bcf9f030dc5b0665
Yenny <107637741+yenny42@users.noreply.github.com>
2024-03-04 11:13:55 +0900

    [Feat] #22 - Tabbar 구현 (#23)
    * [Feat] #22 - 탭바 ViewController 구현
    - 임시 ViewController Class 2개 포함 (추후 삭제)
    
    * [Chore] #22 - 메서드 분리
    
    * [Feat] #22 - SceneDelegate 탭바 설정
    
    * [Chore] #22 - 파일명 변경
    
    * [Chore] #22 - 디자인시스템 버전 업그레이드

---
+++ ToDwoong/ToDwoong/Presentation/TabBar/TabBarController.swift
@@ -0,0 +1,52 @@
+//
+//  TabbarViewController.swift
+//  ToDwoong
+//
+//  Created by yen on 3/3/24.
+//
+
+import UIKit
+
+import TodwoongDesign
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = TDStyle.color.bgBlue
+    }
+}
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = TDStyle.color.bgRed
+    }
+}
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setTabBar()
+    }
+}
+
+extension TabBarController {
+    private func setUI() {
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = TDStyle.color.mainDarkTheme
+    }
+    
+    private func setTabBar() {
+        let toDoListViewController = FirstViewController()
+        toDoListViewController.tabBarItem = UITabBarItem(title: "할 일", image: UIImage(systemName: "checklist"), tag: 0)
+
+        let calendarViewController = SecondViewController()
+        calendarViewController.tabBarItem = UITabBarItem(title: "캘린더", image: UIImage(systemName: "calendar"), tag: 1)
+
+        viewControllers = [toDoListViewController, calendarViewController].map {
+            UINavigationController(rootViewController: $0)
+        }
+    }
+}
