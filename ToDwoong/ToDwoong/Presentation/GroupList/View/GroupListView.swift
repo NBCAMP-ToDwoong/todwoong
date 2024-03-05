cbefe8068e3284305077201288bfaf8b87a05475
Yenny <107637741+yenny42@users.noreply.github.com>
2024-03-05 13:41:39 +0900

    [Feat]#11 - 카테고리 페이지 UI구현 (#40)
    * 셀 오류
    
    셀 오류 관련 코드
    
    * [Feat] #11-카테고리 페이지 UI구현
    
    - 네비게이션 아이템과 타이틀을 작성하고, 색상을 지정했습니다.
    - 테이블뷰를 세팅하고, 편집 버튼을 누를 시 편집 상태(<->노말 상태)로 토글 되게 만들었습니다.
    - 노멀상태에서 셀을 스와이프하면 편집과 삭제가 가능한 기능을 추가하였습니다.
    - 편집 상태에서 셀을 이동시키고, 추가로 삭제메서드를 구현하였습니다.
    
    * [Feat]#11-카테고리 페이지 파일 누락 추가
    
    - 누락시켰던 파일 ContentSizedViewCell을 추가하였습니다.
    
    * [Feat]#11-GroupListViewCell마크 주석 추가
    
    - 마크주석을 추가했습니다.
    
    * [Feat]#11- GroupListViewCell의 확장을 파일로 분리
    
    - extension을 파일을 만들어 작성하였습니다.
    
    * [Feat]#11-GroupListView FIXME주석 추가
    
    - FIXME주석을 추가하였습니다.
    
    * [Feat]#11-GroupListView MARK 주석 추가
    
    - MARK주석을 추가하였습니다.
    
    * [Feat]#11-뷰컨트롤러의 viewDidLoad메서드 추가
    
    - viewController의 viewDidLoad메서드를 다시 작성하였습니다.
    
    * [Feat]#11- GroupListView의
    
    - GroupListView의 extension을 하나로 합쳤습니다.
    
    * [Feat]#11-GroupListView의 setupTableView메서드 이름 수정
    
    - 팀 규칙에 따라 메서드의 이름을 setTableView를 사용했습니다.
    
    * [Feat]#11-GroupListView의 tableView의 스타일 변경
    
    - GroupListView의 tableView의 스타일을 .insetGrouped로 변경하였습니다.
    
    * [Feat]#11- GroupListView의 버튼 간격 추가조정
    
    - GroupListView의 버튼 간격 추가조정을 진행하였습니다.
    
    * [Feat]#11-GroupListTableViewCell에 forEach구문추가
    
    - GroupListTableViewCell에 forEach구문추가하여 작성하였습니다.
    
    * [Feat]#11-GroupListTableViewCell의 메서드화
    
    - GroupListTableViewCell의 레이아웃 관련 설정을 setLayout()메서드로 만들었습니다.
    
    * [Feat]#11-TDStyle에 맞춰서 폰트와 색상수정
    
    - TDStyle을 사용하여 버튼과 폰트의 스타일을 지정하였습니다.
    
    * [Feat]#11-GroupListView의 메서드명 수정
    
    - GroupListView의 메서드명을 수정하였습니다.
    
    * [Feat]#11-GroupListView의 레이아웃설정을 변경하였습니다.
    
    - GroupListView의 레이아웃 설정을 변경하였습니다.
    
    * [Feat]#11-GroupListView의 레이아웃 설정 변경
    
    - GroupListView의 레이아웃을 변경하였습니다.
    
    * [Feat]#11-GroupListView 버튼 높이 수정
    
    - GroupListView의 버튼 레이아웃 높이를 수정하였습니다.
    
    * [Feat]#11-GroupListView TDStyle폰트 적용
    
    - GroupListView TDStyle폰트 적용을 완료했습니다.
    
    * [Feat]#11-ViewController삭제
    
    - ViewController 파일삭제
    
    * Package.resolved
    
    * [Chore] #11 - 린트 에러, 오토레이아웃 재조정
    - 사용하지 않는 파일 삭제
    
    ---------
    
    Co-authored-by: Kdy123 <dudtodtls24@gmail.com>

---
+++ ToDwoong/ToDwoong/Presentation/GroupList/View/GroupListView.swift
@@ -0,0 +1,193 @@
+//
+//  GroupListView.swift
+//  ToDwoong
+//
+//  Created by t2023-m0041 on 3/4/24.
+//
+
+import UIKit
+
+import SnapKit
+import TodwoongDesign
+
+final class GroupListView: UIView {
+    
+    // MARK: - Properties
+    
+    private let normalCellIdentifier = "NormalGroupCell"
+    private let editCellIdentifier = "EditGroupCell"
+    
+    private var dummyCategories: [String] = ["밥먹기", "운동가기", "씻기", "ㅁㄴㅇ", "밥먹기", "운동가기", "씻기"]
+    
+    // MARK: - UI Properties
+    
+    private let tableView: ContentSizedTableView = {
+        let tableView = ContentSizedTableView(frame: .zero, style: .insetGrouped)
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
+    
+    private let addButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("그룹 추가", for: .normal)
+        button.backgroundColor = .white
+        button.setTitleColor(UIColor.systemGray3, for: .normal)
+        button.layer.cornerRadius = 8
+        
+        button.contentHorizontalAlignment = .leading
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 0)
+        
+        if var plusIcon = UIImage(systemName: "plus") {
+            let iconSize = CGSize(width: 15, height: 15)
+            UIGraphicsBeginImageContextWithOptions(iconSize, false, 0.0)
+            plusIcon.draw(in: CGRect(origin: .zero, size: iconSize))
+            plusIcon = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+            UIGraphicsEndImageContext()
+
+            let tintColor = TDStyle.color.bgGray
+            plusIcon = plusIcon.withTintColor(tintColor)
+
+            button.setImage(plusIcon, for: .normal)
+        }
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setAddButton()
+        setTableView()
+        reloadData()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+// MARK: - UI & Layout Extension
+
+extension GroupListView {
+    private func setAddButton() {
+        addSubview(addButton)
+        
+        addButton.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(16)
+            make.trailing.equalToSuperview().offset(-20)
+            make.leading.equalToSuperview().offset(20)
+            make.height.equalTo(44)
+        }
+        
+        addButton.titleLabel?.font = TDStyle.font.body(style: .regular)
+    }
+    
+    private func setTableView() {
+        addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(NormalGroupListTableViewCell.self, forCellReuseIdentifier: normalCellIdentifier)
+        tableView.register(EditGroupListTableViewCell.self, forCellReuseIdentifier: editCellIdentifier)
+    
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(addButton.snp.bottom).offset(-16)
+            make.leading.equalToSuperview().offset(0)
+            make.trailing.equalToSuperview().offset(0)
+        }
+    }
+    
+    private func reloadData() {
+        tableView.reloadData()
+    }
+}
+
+// MARK: UITableViewDelegate
+
+extension GroupListView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView,
+                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath
+    ) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
+            self.deleteCategory(at: indexPath)
+            completion(true)
+        }
+        deleteAction.image = UIImage(systemName: "trash")
+        
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { (action, view, completion) in
+            self.editCategory(at: indexPath)
+            completion(true)
+        }
+        editAction.image = UIImage(systemName: "gear")
+        editAction.backgroundColor = UIColor.orange
+        
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction, editAction])
+        return configuration
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+            return true
+        }
+        
+    func tableView(_ tableView: UITableView,
+                   moveRowAt sourceIndexPath: IndexPath,
+                   to destinationIndexPath: IndexPath) {
+        let movedCategory = dummyCategories.remove(at: sourceIndexPath.row)
+        dummyCategories.insert(movedCategory, at: destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// MARK: UITableViewDataSource
+
+extension GroupListView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyCategories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView.isEditing {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: editCellIdentifier,
+                                                           for: indexPath) as? EditGroupListTableViewCell else {
+                fatalError("셀을 가져오는데 실패하였습니다.")
+            }
+            let category = dummyCategories[indexPath.row]
+            cell.titleLabel.text = category
+            
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: normalCellIdentifier,
+                                                           for: indexPath) as? NormalGroupListTableViewCell else {
+                fatalError("셀을 가져오는데 실패하였습니다.")
+            }
+            let category = dummyCategories[indexPath.row]
+            cell.titleLabel.text = category
+            
+            return cell
+        }
+    }
+}
+
+// MARK: Extension
+
+extension GroupListView {
+    private func editCategory(at indexPath: IndexPath) {
+        // FIXME: 톱니바귀 설정을 눌렀을 때의 메서드를 작성
+    }
+}
+
+extension GroupListView {
+    private func deleteCategory(at indexPath: IndexPath) {
+        dummyCategories.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    func setEditingMode(_ isEditing: Bool) {
+        tableView.setEditing(isEditing, animated: true)
+        tableView.reloadData()
+        tableView.allowsSelectionDuringEditing = isEditing // 셀을 이동하는 동안에도 선택할 수 있도록 설정
+    }
+}
