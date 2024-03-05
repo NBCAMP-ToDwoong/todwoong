12c7a827de0d4dacc33259e155ce6150a3c48263
mirae0312 <ekdtls58@gmail.com>
2024-03-05 20:27:13 +0900

    [Feat]#38 - 캘린더 UI 구현 완료

--- ToDwoong/ToDwoong/Presentation/Calander/ViewController/TodoCalandarController.swift
+++ ToDwoong/ToDwoong/Presentation/Calander/ViewController/TodoCalandarController.swift
@@ -12,17 +12,23 @@ import SnapKit
 import TodwoongDesign
 
 class TodoCalendarController: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
-
+    
+    var todos: [Date]?
+    
     private var calendar: FSCalendar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
         configureCalendar()
         configureCalendarAppearance()
+        fetchEvents()
     }
-
+    
     private func configureCalendar() {
         calendar = FSCalendar()
+        calendar.placeholderType = .none
         calendar.delegate = self
         calendar.dataSource = self
         view.addSubview(calendar)
@@ -33,23 +39,26 @@ class TodoCalendarController: UIViewController, FSCalendarDelegate, FSCalendarDa
         }
     }
     
-    func calendar(_ calendar: FSCalendar, shouldDisplayCellForDate date: Date, atMonthPosition position: FSCalendarMonthPosition) -> Bool {
-        return position == .current
-    }
-
     private func configureCalendarAppearance() {
-        calendar.appearance.headerDateFormat = "MMMM yyyy"
+        calendar.locale = Locale(identifier: "ko_KR")
+        calendar.appearance.headerDateFormat = "yyyy MMMM"
         calendar.appearance.headerTitleColor = TDStyle.color.mainDarkTheme
         calendar.appearance.weekdayTextColor = .systemGray3
         calendar.appearance.titleDefaultColor = .black
         calendar.appearance.selectionColor = UIColor(red: 230/255, green: 244/255, blue: 237/255, alpha: 1)
-        calendar.appearance.titleTodayColor = TDStyle.color.mainDarkTheme
-        calendar.appearance.todayColor = UIColor.clear
+        
+        // 오늘 날짜의 스타일을 설정
+        calendar.appearance.titleTodayColor = .white // 오늘 날짜의 글자색
+        calendar.appearance.todayColor = TDStyle.color.mainDarkTheme // 오늘 날짜의 배경색
         calendar.appearance.titleSelectionColor = UIColor.black
+        
+        // 폰트 설정
+        calendar.appearance.titleFont = TDStyle.font.body(style: .regular) // 모든 날짜에 대한 볼드체 폰트 설정
+        
         calendar.appearance.eventDefaultColor = UIColor.black
         calendar.appearance.eventSelectionColor = TDStyle.color.mainDarkTheme
+        calendar.appearance.headerMinimumDissolvedAlpha = 0.0
         calendar.backgroundColor = .white
-        calendar.placeholderType = .none
         
         // 화살표 버튼을 캘린더 뷰에 추가
         let prevMonthButton = UIButton(type: .system)
@@ -89,5 +98,26 @@ class TodoCalendarController: UIViewController, FSCalendarDelegate, FSCalendarDa
         guard let nextMonth = Calendar.current.date(byAdding: .month, value: 1, to: self.calendar.currentPage) else { return }
         self.calendar.setCurrentPage(nextMonth, animated: true)
     }
-
+    
+    func fetchEvents() {
+        let dateMatter = DateFormatter()
+        dateMatter.locale = Locale(identifier: "ko_KR")
+        dateMatter.dateFormat = "yyyy-MM-dd"
+        
+        // events
+        let myFirstEvent = dateMatter.date(from: "2024-03-10")
+        let mySecondEvent = dateMatter.date(from: "2024-03-20")
+        
+        // 두 날짜를 배열에 추가. 옵셔널 바인딩을 사용하여 nil을 확인
+        todos = [myFirstEvent, mySecondEvent].compactMap { $0 }
+    }
+    
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        guard let events = todos else { return 0 }
+        if events.contains(where: { Calendar.current.isDate($0, inSameDayAs: date) }) {
+            return 1 // 이 날짜에 이벤트가 있으면 점을 하나 표시
+        }
+        return 0
+    }
+    
 }
