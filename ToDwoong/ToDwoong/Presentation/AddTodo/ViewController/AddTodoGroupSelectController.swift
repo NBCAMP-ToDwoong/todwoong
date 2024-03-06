12d4e92aea3c4004497bec9b9f56d44ef5cdce09
Yenny <107637741+yenny42@users.noreply.github.com>
2024-03-06 11:53:53 +0900

    [Feat] #46 - 투두 추가 페이지 그룹 모달 (#53)
    * [Feat] #46 - 투두 추가 그룹 모달 구현
    
    * [Feat] #46 - 그룹모달 풀스크린 해제

--- ToDwoong/ToDwoong/Presentation/AddTodo/ViewController/AddTodoGroupSelectController.swift
+++ ToDwoong/ToDwoong/Presentation/AddTodo/ViewController/AddTodoGroupSelectController.swift
@@ -8,16 +8,163 @@
 import UIKit
 
 import SnapKit
+import TodwoongDesign
 
-class AddTodoGroupSelectController: UIViewController {
+final class AddTodoGroupSelectController: UIViewController {
     
-    // MARK: UI Properties
+    // MARK: - Properties
     
     weak var delegate: AddTodoGroupSelectControllerDelegate?
+    private let groupList = ["test1", "test2", "test3"]
+    private var selectGroup = ""
+    
+    // MARK: - UI Properties
+    
+    private lazy var timeNotificationButton: UIButton = {
+        var config = UIButton.Configuration.plain()
+        
+        config.title = "그룹"
+        config.baseForegroundColor = TDStyle.color.mainDarkTheme
+        config.baseBackgroundColor = .clear
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: TDStyle.font.body(style: .bold)
+        ]
+        config.attributedTitle = AttributedString("그룹", attributes: AttributeContainer(attributes))
+        
+        let button = UIButton(configuration: config, primaryAction: nil)
+        
+        return button
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        createNotificationButton(title: "저장", method: #selector(saveGroup), color: .systemBlue)
+    }()
+    
+    private func createNotificationButton(title: String,
+                                          method: Selector,
+                                          color: UIColor? = TDStyle.color.primaryLabel
+    ) -> UIButton {
+        var config = UIButton.Configuration.plain()
+        
+        config.title = title
+        config.baseForegroundColor = color
+        config.baseBackgroundColor = .clear
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: TDStyle.font.body(style: .regular)
+        ]
+        config.attributedTitle = AttributedString(title, attributes: AttributeContainer(attributes))
+        
+        let button = UIButton(configuration: config, primaryAction: nil)
+        button.addTarget(self, action: method, for: .touchUpInside)
+        
+        return button
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.backgroundColor = .white
+        tableView.contentInset = UIEdgeInsets(top: -35, left: 0, bottom: 0, right: 0)
+        
+        return tableView
+    }()
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        setTableView()
+        setUI()
+        setLayout()
+    }
+}
+
+// MARK: - @objc Method
+
+extension AddTodoGroupSelectController {
+    @objc
+    func saveGroup() {
+        delegate?.groupSelectController(self, didSelectGroup: selectGroup)
+    }
+}
+
+// MARK: - UI Method
+
+extension AddTodoGroupSelectController {
+    private func setUI() {
+        [
+            timeNotificationButton,
+            tableView,
+            saveButton
+        ].forEach {
+            view.addSubview($0)
+        }
+        
+        view.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        timeNotificationButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.equalToSuperview().offset(10)
+        }
+        
+        saveButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.trailing.equalToSuperview().offset(-10)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(timeNotificationButton.snp.bottom).offset(16)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "groupTableViewCellIdentifier")
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension AddTodoGroupSelectController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groupList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "groupTableViewCellIdentifier", for: indexPath)
+        cell.textLabel?.text = groupList[indexPath.row]
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension AddTodoGroupSelectController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.selectionStyle = .none
+            cell.accessoryType = .checkmark
+            
+            selectGroup = groupList[indexPath.row]
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
 }
