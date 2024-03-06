d39515c9befdcb1cf880e2dff82572d815693b4f
Kdy123 <dudtodtls24@gmail.com>
2024-03-07 05:03:24 +0900

    [Feat]#52-카테고리 색상을 받아오기위한 확장 코드

---
+++ ToDwoong/ToDwoong/Presentation/Map/View/TodoAnnotation.swift
@@ -0,0 +1,21 @@
+//
+//  TodoAnnotation.swift
+//  ToDwoong
+//
+//  Created by t2023-m0041 on 3/7/24.
+//
+
+import MapKit
+import UIKit
+
+class TodoAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var colorName: String
+    
+    init(coordinate: CLLocationCoordinate2D, title: String, colorName: String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.colorName = colorName
+    }
+}
