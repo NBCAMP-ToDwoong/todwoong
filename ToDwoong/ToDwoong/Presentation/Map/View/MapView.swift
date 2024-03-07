6e64468374460bccacd45ce9acda9790daea833a
Kdy123 <dudtodtls24@gmail.com>
2024-03-07 17:05:51 +0900

    [Feat]#52-지도페이지 기능 구현 테스트를 위해서 지도, 카테고리칩버튼, 모달창(테이블뷰)을 만들었습니다.

--- ToDwoong/ToDwoong/Presentation/Map/View/MapView.swift
+++ ToDwoong/ToDwoong/Presentation/Map/View/MapView.swift
@@ -5,4 +5,60 @@
 //  Created by yen on 3/6/24.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+import SnapKit
+import TodwoongDesign
+
+class MapView: UIView {
+    
+    let mapView: MKMapView = MKMapView()
+    private let scrollView: UIScrollView = UIScrollView()
+    let stackView: UIStackView = UIStackView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setMapView()
+        setCategoryChipsView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setMapView() {
+        addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func setCategoryChipsView() {
+        addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(10)
+            make.leading.trailing.equalTo(self)
+            make.height.equalTo(44)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(scrollView)
+            make.leading.trailing.equalTo(scrollView.contentLayoutGuide)
+            make.height.equalTo(scrollView)
+        }
+    }
+    
+    func addCategoryChip(category: DummyCategory, action: Selector, target: Any?) {
+        let chipButton = TDCustomButton(frame: .zero, type: .chip, title: category.title, backgroundColor: TDStyle.color.colorFromString(category.color) ?? .gray)
+        chipButton.addTarget(target, action: action, for: .touchUpInside)
+        stackView.addArrangedSubview(chipButton)
+    }
+}
---
+++ ToDwoong/ToDwoong/Presentation/Map/View/TodoDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  TodoDetailView.swift
+//  ToDwoong
+//
+//  Created by t2023-m0041 on 3/7/24.
+//
+
+import UIKit
+import SnapKit
+import TodwoongDesign
+
+class TodoDetailView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.register(TDTableViewCell.self, forCellReuseIdentifier: TDTableViewCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = TDStyle.color.bgGray
+        tableView.backgroundColor = TDStyle.color.bgGray
+        setTableView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setTableView() {
+        addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(30)
+            make.bottom.equalToSuperview().offset(-10)
+            make.leading.equalToSuperview().offset(15)
+            make.trailing.equalToSuperview().offset(-15)
+        }
+    }
+}
