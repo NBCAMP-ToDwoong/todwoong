9e8ae71fa8258af09e22df8860e57236be1f8158
codeIsGone <151711359+codeIsGone@users.noreply.github.com>
2024-03-08 15:50:00 +0900

    [Feat] #37 - 온보딩, 런치스크린 페이지 UI 및 분기 구현 (#77)
    * [Feat] #37 - 런치스크린 연결, 최초 접근 분기처리 구현
    
    * [Feat] #37 - 온보딩 페이지 레이아웃 변경, 임시 이미지 적용
    
    * [Chore] #37 - final 키워드 추가
    
    * [Chore] #37 - 임시 이미지에서 확정 이미지로 변경
    
    * [Chore] #37 - Asset 이미지 이름 변경

---
+++ ToDwoong/ToDwoong/Presentation/LaunchScreen/View/LaunchScreenView.swift
@@ -0,0 +1,51 @@
+//
+//  LaunchScreenView.swift
+//  ToDwoong
+//
+//  Created by 홍희곤 on 3/8/24.
+//
+
+import UIKit
+
+import SnapKit
+import TodwoongDesign
+
+final class LaunchScreenView: UIView {
+    
+    // MARK: UI Properties
+    
+    private let launchScreenImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "appIcon")
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    // MARK: - Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+}
+
+// MARK: - Extension
+
+extension LaunchScreenView {
+    private func setUI() {
+        backgroundColor = TDStyle.color.mainTheme
+        addSubview(launchScreenImageView)
+        
+        launchScreenImageView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.height.width.equalTo(self.snp.width).dividedBy(3)
+        }
+    }
+}
