ba38deb3fc8997abc7aefb660a93f0538d2294a6
codeIsGone <151711359+codeIsGone@users.noreply.github.com>
2024-03-08 15:50:00 +0900

    [Feat] #37 - 온보딩, 런치스크린 페이지 UI 및 분기 구현 (#77)
    * [Feat] #37 - 런치스크린 연결, 최초 접근 분기처리 구현
    
    * [Feat] #37 - 온보딩 페이지 레이아웃 변경, 임시 이미지 적용
    
    * [Chore] #37 - final 키워드 추가
    
    * [Chore] #37 - 임시 이미지에서 확정 이미지로 변경
    
    * [Chore] #37 - Asset 이미지 이름 변경

---
+++ ToDwoong/ToDwoong/Presentation/LaunchScreen/ViewController/LaunchScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenViewController.swift
+//  ToDwoong
+//
+//  Created by 홍희곤 on 3/8/24.
+//
+
+import UIKit
+
+final class LaunchScreenViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+    
+    override func loadView() {
+        view = LaunchScreenView()
+    }
+}
