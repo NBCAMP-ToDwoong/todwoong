7377958c3383c3ddde24a666f7fb51b39874fb59
Kdy123 <123481645+Whited123@users.noreply.github.com>
2024-03-08 17:21:59 +0900

    Feat/#52-지도 페이지 기능 구현(UI일부) (#79)
    * [Feat] #52 - Map 파일 생성
    
    * [Feat]#52-MapView지도 페이지 구현
    
    * [Feat]#52-지도페이지 현재위치 조회 기능, 페이지 진입 시 투두의 각 장소에 핀을 생성하는 기능입니다.
    
    * [Feat]#52-핀을 선택시 줌인되는 기능
    
    * [Feat]#52-지도페이지 핀 클릭시 모달창 생성기능 추가
    
    * [Feat]#52-지도페이지 로드시 Todo의 Category색상에 따라 핀의 색상을 바꾸는 기능 추가 + 위치 기반 메서드 작성
    
    * [Feat]#52-모달창의 테이블뷰 DataSource와 Delegate 기능 구현
    
    * [Feat]#52-카테고리 색상을 받아오기위한 확장 코드
    
    * project file
    
    * [Feat]#52-주석처리 추가입니다.
    
    * [Feat]#52-지도페이지 핀을 카테고리의 color별로 색을 입히고 클릭시 모달창을 띄우는 기능을 구현
    
    * [Feat]#52-지도페이지 기능 구현 테스트를 위해서 지도, 카테고리칩버튼, 모달창(테이블뷰)을 만들었습니다.
    
    * [Feat]#52-카테고리칩의 선택에 따라 모달창에 표시되는 데이터를 필터하는 기능입니다.
    
    * [Feat]#52-핀을 선택하면 해당 핀의 카테고리에 속하는 Todo를 셀에표시하는 기능추가
    
    * [Feat]#52-지도페이지 핀이나 칩 선택시 셀을 필터해서 보여주는 기능을 추가하고 네비게이션도 추가했습니다.
    
    * [Feat]#52-주석처리추가입니다.
    
    ---------
    
    Co-authored-by: yenny <yennydev42@gmail.com>

---
+++ ToDwoong/ToDwoong/Presentation/Map/ViewController/MapViewController.swift
@@ -0,0 +1,294 @@
+//
+//  MapViewController.swift
+//  ToDwoong
+//
+//  Created by yen on 3/6/24.
+//
+
+import CoreLocation
+import MapKit
+import UIKit
+
+import TodwoongDesign
+
+class MapViewController: UIViewController {
+    
+    // MARK: - Instance
+    
+    var customMapView: MapView!
+    // 핀 크기관련 프로퍼티... 현재 작동하지않음
+//    var selectedAnnotation: MKAnnotation?
+    let locationManager = CLLocationManager()
+    
+    // MARK: - Data Storage
+    
+    var todos: [TodoModel] = []
+    var categories: [CategoryModel] = []
+    var todoAnnotationMap: [String: TodoAnnotation] = [:]
+    
+    // MARK: - Lifecycle
+    
+    override func loadView() {
+        customMapView = MapView()
+        view = customMapView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        customMapView.mapView.delegate = self
+        setLocationManager()
+        setNavigationBar()
+        createDummyData()
+    }
+    
+    // MARK: - Setting Method
+    
+    private func setLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func setNavigationBar() {
+        let leftBarButtonItem = UIBarButtonItem(
+            title: "< Back",
+            style: .plain,
+            target: self,
+            action: #selector(didTapBackButton)
+        )
+        navigationItem.leftBarButtonItem = leftBarButtonItem
+        
+        let rightBarButtonItem = UIBarButtonItem(
+            title: "내 위치",
+            style: .plain,
+            target: self,
+            action: #selector(moveToCurrentLocation)
+        )
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        // 네비게이션 색상 설정
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithTransparentBackground()
+        navigationBarAppearance.backgroundColor = .clear
+        
+        navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+    }
+    
+    // 더미데이터 생성코드
+    func createDummyData() {
+        let allCategory = CategoryModel(id: UUID(), title: "전체", color: "bgGray", indexNumber: 0, todo: nil)
+        let workCategory = CategoryModel(id: UUID(), title: "일", color: "bgRed", indexNumber: 1, todo: nil)
+        let personalCategory = CategoryModel(id: UUID(), title: "운동", color: "bgBlue", indexNumber: 2, todo: nil)
+        categories = [workCategory, personalCategory]
+        categories.insert(allCategory, at: 0)
+
+        let todos = [
+            TodoModel(id: UUID(), title: "프로젝트 끝내기",
+                      dueDate: Date(),
+                      dueTime: Date(),
+                      place: "서울",
+                      isCompleted: false,
+                      fixed: false,
+                      timeAlarm: true,
+                      placeAlarm: true,
+                      category: workCategory),
+            TodoModel(id: UUID(), title: "운동 가기",
+                      dueDate: Date().addingTimeInterval(86400 * 2),
+                      dueTime: Date(), place: "부산",
+                      isCompleted: false,
+                      fixed: false,
+                      timeAlarm: true,
+                      placeAlarm: true,
+                      category: personalCategory),
+            TodoModel(id: UUID(), title: "TIL작성하기",
+                      dueDate: Date(),
+                      dueTime: Date(),
+                      place: "대구",
+                      isCompleted: false,
+                      fixed: false,
+                      timeAlarm: true,
+                      placeAlarm: true,
+                      category: workCategory),
+            TodoModel(id: UUID(), title: "밥먹기",
+                      dueDate: Date().addingTimeInterval(86400 * 2),
+                      dueTime: Date(), place: "대전",
+                      isCompleted: false,
+                      fixed: false,
+                      timeAlarm: true,
+                      placeAlarm: true,
+                      category: personalCategory)
+        ]
+
+        self.todos = todos
+
+        loadCategoriesAndCategoryChips()
+        loadTodosAndPins()
+    }
+    
+    // Todo데이터를 불러오고, 각 투두의 장소에 맞는 핀을 생성하는 메서드를 실행하는 메서드
+    private func loadTodosAndPins() {
+        for todo in self.todos {
+            if let place = todo.place, let categoryColor = todo.category?.color {
+                addPinForPlace(place, colorName: categoryColor, category: todo.category?.title ?? "", todo: todo)
+            }
+        }
+    }
+
+    func addPinForPlace(_ place: String, colorName: String, category: String, todo: TodoModel) {
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(place) { [weak self] (placemarks, error) in
+            guard let strongSelf = self else { return }
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            if let placemark = placemarks?.first, let location = placemark.location {
+                let annotation = TodoAnnotation(coordinate: location.coordinate, title: place, colorName: colorName, category: category)
+                
+                strongSelf.todoAnnotationMap[todo.id?.uuidString ?? ""] = annotation
+                
+                DispatchQueue.main.async {
+                    strongSelf.customMapView.mapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
+    
+    private func loadCategoriesAndCategoryChips() {
+        categories.forEach { category in
+            customMapView.addCategoryChip(category: category, action: #selector(categoryChipTapped(_:)), target: self)
+        }
+    }
+    
+    // MARK: - Objc Func
+    
+    @objc func categoryChipTapped(_ sender: TDCustomButton) {
+        guard let title = sender.titleLabel?.text else { return }
+        
+        if title == "전체" {
+            customMapView.mapView.annotations.forEach { annotation in
+                guard let todoAnnotation = annotation as? TodoAnnotation else { return }
+                customMapView.mapView.view(for: annotation)?.isHidden = false
+            }
+        } else {
+            customMapView.mapView.annotations.forEach { annotation in
+                guard let todoAnnotation = annotation as? TodoAnnotation else { return }
+                customMapView.mapView.view(for: annotation)?.isHidden = todoAnnotation.category != title
+            }
+        }
+        
+        let detailVC = TodoDetailViewController()
+        detailVC.selectedCategoryTitle = title
+        detailVC.todos = self.todos
+        detailVC.modalPresentationStyle = .pageSheet
+        
+        if let sheet = detailVC.sheetPresentationController {
+            sheet.prefersGrabberVisible = true
+            sheet.detents = [.medium(), .large()]
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+        }
+        
+        present(detailVC, animated: true, completion: nil)
+    }
+    
+    @objc private func didTapBackButton() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // 버튼을 눌렀을 때, 사용자의 현재 위치로 돌아가는 메서드
+    @objc func moveToCurrentLocation() {
+        if let currentLocation = locationManager.location {
+            let region = MKCoordinateRegion(center: currentLocation.coordinate,
+                                            latitudinalMeters: 300,
+                                            longitudinalMeters: 300)
+            customMapView.mapView.setRegion(region, animated: true)
+        }
+    }
+}
+
+// Extension
+
+// MARK: - CLLocationManagerDelegate
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, latitudinalMeters: 300, longitudinalMeters: 300)
+            customMapView.mapView.setRegion(region, animated: true)
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
+    }
+}
+
+// MARK: - MKMapViewDelegate
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if let coordinate = view.annotation?.coordinate {
+            let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 300, longitudinalMeters: 300)
+            mapView.setRegion(region, animated: true)
+//            selectedAnnotation = view.annotation
+        }
+        
+        let detailVC = TodoDetailViewController()
+        detailVC.modalPresentationStyle = .pageSheet
+        
+        if let todoAnnotation = view.annotation as? TodoAnnotation {
+            detailVC.selectedCategoryTitle = todoAnnotation.category
+            detailVC.todos = self.todos
+        }
+        
+        if let sheet = detailVC.sheetPresentationController {
+            sheet.prefersGrabberVisible = true
+            sheet.detents = [.medium(), .large()]
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+        }
+        
+        present(detailVC, animated: true, completion: nil)
+    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let todoAnnotation = annotation as? TodoAnnotation {
+            let reuseId = "marker"
+            var markerView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKMarkerAnnotationView
+            
+            if markerView == nil {
+                markerView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+                markerView?.canShowCallout = true
+            } else {
+                markerView?.annotation = annotation
+            }
+
+            let color = TDStyle.color.colorFromString(todoAnnotation.colorName)
+            markerView?.markerTintColor = color ?? UIColor.green
+
+            return markerView
+        }
+
+        return nil
+    }
+    
+//    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//        if let selectedAnnotation = selectedAnnotation,
+//           let annotationView = mapView.view(for: selectedAnnotation) as? MKMarkerAnnotationView {
+//            let color = TDStyle.color.colorFromString((selectedAnnotation as? TodoAnnotation)?.colorName ?? "")
+//            annotationView.markerTintColor = color ?? UIColor.green
+//            annotationView.transform = CGAffineTransform.identity
+//            self.selectedAnnotation = nil
+//        }
+//    }
+}
