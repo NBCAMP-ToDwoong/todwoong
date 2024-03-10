15ea1468dc960aecc3fca2e5a843037f6dc6ae7f
yenny <yennydev42@gmail.com>
2024-03-11 05:10:18 +0900

    [Chore] #73 - 투두리스트 그룹 필터 임시작업

--- ToDwoong/ToDwoong/Presentation/TodoList/ViewController/TodoListViewController.swift
+++ ToDwoong/ToDwoong/Presentation/TodoList/ViewController/TodoListViewController.swift
@@ -148,7 +148,9 @@ extension TodoListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         // FIXME: 그룹 필터 로직 구현 예정
-        
+        selectedGroup = indexPath.row
+        todoView.groupCollectionView.reloadData()
+        todoView.todoTableView.reloadData()
     }
 }
 
