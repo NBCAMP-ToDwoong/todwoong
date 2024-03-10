2895f2f347e2c50fd2b089c6305dbce485cdc9f7
yenny <yennydev42@gmail.com>
2024-03-10 21:30:13 +0900

    [Chore] #99 - 잘못 분리된 Extension 삭제

--- ToDwoong/ToDwoong/Presentation/Map/ViewController/MapViewController.swift
+++ ToDwoong/ToDwoong/Presentation/Map/ViewController/MapViewController.swift
@@ -321,3 +321,20 @@ extension MapViewController {
         present(detailVC, animated: true, completion: nil)
     }
 }
+
+extension MapViewController {
+    func zoomToTodo(_ todo: TodoModel, completion: @escaping () -> Void) {
+        if let annotation = todoAnnotationMap[todo.id?.uuidString ?? ""] {
+            let region = MKCoordinateRegion(center: annotation.coordinate,
+                                            latitudinalMeters: 300,
+                                            longitudinalMeters: 300)
+            customMapView.mapView.setRegion(region, animated: true)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+}
