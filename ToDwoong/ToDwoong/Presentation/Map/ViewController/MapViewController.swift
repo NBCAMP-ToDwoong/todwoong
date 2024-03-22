9654a820bc1adc2bdb45717b4ecd91df88d8ff89
yenny <yennydev42@gmail.com>
2024-03-22 16:02:24 +0900

    [Feat] #153 - 핀 이미지 변경 - 어노테이션 콜아웃으로 변경

--- ToDwoong/ToDwoong/Presentation/Map/ViewController/MapViewController.swift
+++ ToDwoong/ToDwoong/Presentation/Map/ViewController/MapViewController.swift
@@ -52,6 +52,7 @@ extension MapViewController {
         mapView.showsUserLocation = true
         mapView.delegate = self
         mapView.isRotateEnabled = false
+        mapView.isPitchEnabled = false
     }
     
     func setNavigationItem() {
@@ -98,27 +99,46 @@ extension MapViewController {
     }
 }
 
-// MARK: - Objc Method
+// MARK: - MapView Pin
 
-extension MapViewController: MKMapViewDelegate {
-    
+extension MapViewController {
     func addPinsToMap() {
-        // 예시 위치 핀 추가
-        let pinLocation = CLLocationCoordinate2D(latitude: 37.5665, longitude: 126.9780)
-        let pin = MKPointAnnotation()
-        pin.coordinate = pinLocation
-        pin.title = "서울 시청"
-        mapView.addAnnotation(pin)
+        // 예시 위치 핀
+        let pin = createAnnotation(title: "서울 시청에서 레쓰비 고소하기",
+                                   latitude: 37.5665,
+                                   longitude: 126.9780, 
+                                   pinColor: TDStyle.color.bgBlue)
+        let pin2 = createAnnotation(title: "부산",
+                                    latitude: 35.1795,
+                                    longitude: 129.0756, 
+                                    pinColor: nil)
         
-        let pinLocation2 = CLLocationCoordinate2D(latitude: 35.1795, longitude: 129.0756)
-        let pin2 = MKPointAnnotation()
-        pin2.coordinate = pinLocation2
-        pin2.title = "부산"
-        mapView.addAnnotation(pin2)
-        
+        [pin, pin2].forEach {
+            mapView.addAnnotation($0)
+        }
+            
         // 모든 핀이 보이도록 지도 범위 조정
         mapView.showAnnotations([pin, pin2], animated: true)
     }
+    
+
+    func createAnnotation(title: String, latitude: Double, longitude: Double, pinColor: UIColor?) -> ColoredAnnotation {
+        let annotation = ColoredAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        annotation.title = title
+        annotation.pinColor = pinColor ?? TDStyle.color.mainTheme
+        
+        return annotation
+    }
+}
+
+// MARK: - MKMapViewDelegate
+
+class ColoredAnnotation: MKPointAnnotation {
+    var pinColor: UIColor?
+}
+
+extension MapViewController: MKMapViewDelegate {
     
     // 핀 클릭 시 확대
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
@@ -126,13 +146,13 @@ extension MapViewController: MKMapViewDelegate {
         
         let offset = -0.005
         let newCenterLatitude = annotation.coordinate.latitude + offset
-        let newCenter = CLLocationCoordinate2D(latitude: newCenterLatitude, longitude: annotation.coordinate.longitude)
+        let newCenter = CLLocationCoordinate2D(latitude: newCenterLatitude, 
+                                               longitude: annotation.coordinate.longitude)
         
         let region = MKCoordinateRegion(center: newCenter,
                                         latitudinalMeters: regionRadius,
                                         longitudinalMeters: regionRadius)
         mapView.setRegion(region, animated: true)
-        
         
         if let sheet = todoDetailViewController.sheetPresentationController {
             sheet.detents = [.medium(), .large()]
@@ -140,5 +160,63 @@ extension MapViewController: MKMapViewDelegate {
         }
         present(todoDetailViewController, animated: true, completion: nil)
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let coloredAnnotation = annotation as? ColoredAnnotation else {
+            return nil
+        }
 
+        let reuseId = "customPin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        
+        if pinView == nil {
+            pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+        } else {
+            pinView?.annotation = annotation
+        }
+        
+        if let customImage = UIImage(named: "AddTodoMapPin")?.withRenderingMode(.alwaysTemplate) {
+            let resizedImage = resizeImage(image: customImage, targetSize: CGSize(width: 26, height: 26))
+            let coloredAndResizedImage = resizedImage.withTintColor(
+                coloredAnnotation.pinColor ?? TDStyle.color.mainTheme)
+            pinView?.image = coloredAndResizedImage
+            
+            pinView?.layer.shadowColor = UIColor.black.cgColor
+            pinView?.layer.shadowOpacity = 0.8
+            pinView?.layer.shadowOffset = CGSize(width: 0, height: 1)
+            pinView?.layer.shadowRadius = 2
+        }
+
+        return pinView
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        let resizedImage = renderer.image { _ in
+            image.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+        return resizedImage
+    }
+
+}
+extension UIImage {
+    func withTintColor(_ color: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        color.set()
+        
+        let context = UIGraphicsGetCurrentContext()
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        context?.setBlendMode(.normal)
+        
+        let rect = CGRect(origin: .zero, size: self.size)
+        context?.clip(to: rect, mask: self.cgImage!)
+        context?.fill(rect)
+        
+        let tintedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return tintedImage ?? self
+    }
 }
