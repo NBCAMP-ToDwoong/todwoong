b1cb1f9520a20a113f15d632dfc1422564f24323
yenny <yennydev42@gmail.com>
2024-03-27 21:13:34 +0900

    [Fix] #184 - 그룹별 필터 추가

--- ToDwoong/ToDwoong/Presentation/Map/ViewController/MapViewController.swift
+++ ToDwoong/ToDwoong/Presentation/Map/ViewController/MapViewController.swift
@@ -31,6 +31,7 @@ final class MapViewController: UIViewController {
     
     private lazy var buttonAction: ((UIButton) -> Void) = { [weak self] button in
         guard let self = self else { return }
+        
         selectedGroup = button.tag
         mapView.allGroupButton.alpha = 0.3
         openTodoListModal(name: button.titleLabel?.text!)
@@ -139,9 +140,11 @@ extension MapViewController {
     }
     
     private func updateMapAnnotations() {
-        DispatchQueue.main.async {
-            self.mapView.mapView.removeAnnotations(self.mapView.mapView.annotations) // 기존 핀 제거
-            self.addPinsToMap(todos: self.allTodoList) // 새로운 핀 추가
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            
+            self.mapView.mapView.removeAnnotations(self.mapView.mapView.annotations)
+            self.addPinsToMap(todos: self.allTodoList)
         }
     }
 }
@@ -183,21 +186,28 @@ extension MapViewController {
 // MARK: - MapView Pin
 extension MapViewController {
     private func addPinsToMap(todos: [TodoType]) {
-        self.pins = []
-        for todo in todos {
-            if let latitude = todo.placeAlarm?.latitude,
-               let longitude = todo.placeAlarm?.longitude {
-                let pin = createAnnotation(title: todo.title,
-                                           latitude: latitude,
-                                           longitude: longitude,
-                                           pinColor: UIColor.blue) // 예시 색상
-                
-                mapView.mapView.addAnnotation(pin)
-                pins.append(pin)
+        self.pins.removeAll()
+        
+        let filteredTodos: [TodoType]
+        if let selectedGroupIndex = self.selectedGroup {
+            let selectedGroupTitle = self.groupList[selectedGroupIndex].title
+            filteredTodos = self.allTodoList.filter { $0.group?.title == selectedGroupTitle }
+        } else {
+            filteredTodos = self.allTodoList
+        }
+        
+        for todo in filteredTodos {
+            if let latitude = todo.placeAlarm?.latitude, let longitude = todo.placeAlarm?.longitude {
+                let pin = self.createAnnotation(title: todo.title,
+                                                latitude: latitude,
+                                                longitude: longitude,
+                                                pinColor: UIColor(hex: todo.group?.color ?? "#000000"))
+                self.mapView.mapView.addAnnotation(pin)
+                self.pins.append(pin)
             }
         }
         
-        mapView.mapView.showAnnotations(pins, animated: true)
+        self.mapView.mapView.showAnnotations(self.pins, animated: true)
     }
     
     private func createAnnotation(title: String,
